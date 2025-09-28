10e456c1de7b6789830832e6ad49072d539e3348
Kayra <enelminun@protonmail.com>
2025-09-28 08:59:37 +0300

    Update proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -4,8 +4,6 @@
 
 #include <stdint.h>
 
-// Basit process exec API'si
-// ELF dosyasını açar, memory map yapar ve kullanıcı modunda çalıştırır.
 int proc_exec_from_elf(const char *path);
 
 // İleride eklenebilecekler:
