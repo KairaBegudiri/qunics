71bedcbc898a28681754c4b7a2729cd79dd01f3a
Kayra <enelminun@protonmail.com>
2025-09-28 08:58:39 +0300

    Update kstdio.h

--- kernel/kstdio.h
+++ kernel/kstdio.h
@@ -5,13 +5,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// Tek karakter yazdır
 void vga_putc(char c);
 
-// String yazdır
 void kputs(const char* s);
 
-// Formatlı çıktı (%s, %d, %u, %p, %c)
 void kprintf(const char* fmt, ...);
 
-#endif // KSTDIO_H
+#endif
